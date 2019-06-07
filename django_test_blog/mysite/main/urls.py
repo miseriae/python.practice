160f573f1a2069b50b1c27d932ed039bca04178c
miseriae <zuyev.frm@gmail.com>
2019-06-07 17:24:15 +0300

    Added and modified register template

--- django_test_blog/mysite/main/urls.py
+++ django_test_blog/mysite/main/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.homepage, name="homepage")
+    path('', views.homepage, name="homepage"),
+    path('register/', views.register, name="register"),
 ]
