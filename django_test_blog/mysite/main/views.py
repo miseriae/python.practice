3f36061b4c8b7035dbca40f0ecdcdeece28c20d9
miseriae <zuyev.frm@gmail.com>
2019-06-07 18:18:27 +0300

    updated main/views

--- django_test_blog/mysite/main/views.py
+++ django_test_blog/mysite/main/views.py
@@ -3,6 +3,7 @@ from .models import BlogPost
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 
 def homepage(request):
@@ -15,12 +16,15 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account Created: {username}")
             login(request, user)
+            messages.info(request, f"Logged in as: {username}")
             return redirect("main:homepage")
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
-                
+                messages.error(request, f"{msg}: {form.error_messages[msg]}")
+
 
     form = UserCreationForm
     return render(request=request,
