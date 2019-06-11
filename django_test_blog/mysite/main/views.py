c58a79bd3ea510b6bc428f63e8a591f383e77d45
miseriae <zuyev.frm@gmail.com>
2019-06-11 20:11:18 +0300

    added login/logout functions

--- django_test_blog/mysite/main/views.py
+++ django_test_blog/mysite/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import BlogPost
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
@@ -30,3 +30,29 @@ def register(request):
     return render(request=request,
                   template_name="main/register.html",
                   context={"form": form})
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("main:homepage")
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as: {username}")
+                return redirect("main:homepage")
+            else:
+                messages.error(request, "Invalid username or password")
+        else:
+            messages.error(request, "Invalid username or password")
+
+    form = AuthenticationForm()
+    return render(request=request,
+                  template_name="main/login.html",
+                  context={"form": form})    
