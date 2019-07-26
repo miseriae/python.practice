76cf9c26782b33855d8ac3f86d5cf434189aaf77
miseriae <zuyev.frm@gmail.com>
2019-07-26 18:20:36 +0300

    Modify homepage

--- django_app/mysite/main/views.py
+++ django_app/mysite/main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return HttpResponse("<h1> Homepage <h2>")
+    return HttpResponse("<h1><strong>Blog Homepage</strong></h1>")
