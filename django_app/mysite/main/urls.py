3165f2c5d6aef3c8c8ec7664f4bb3aac1ebf6e8a
miseriae <zuyev.frm@gmail.com>
2019-07-26 18:34:03 +0300

    Update homepage view

--- django_app/mysite/main/urls.py
+++ django_app/mysite/main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'main'
 
 urlpatterns = [
     path('', views.homepage, name='blog-homepage'),
+    path('about/', views.about, name='blog-about'),
 ]
--- django_app/mysite/main/views.py
+++ django_app/mysite/main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def homepage(request):
     return HttpResponse("<h1><strong>Blog Homepage</strong></h1>")
+
+
+def about(request):
+    return HttpResponse("<h1>About Blog</h1>")
