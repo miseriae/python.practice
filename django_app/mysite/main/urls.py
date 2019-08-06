e97d0770d782af9f61948e0d436c17cce0c75e07
miseriae <zuyev.frm@gmail.com>
2019-08-06 15:53:16 +0300

    Urls for Create-Update-Delete

--- django_app/mysite/main/urls.py
+++ django_app/mysite/main/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.homepage, name='main-homepage'),
+    path('', PostListView.as_view(), name='main-homepage'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='main-about'),
 ]
+
+
+
+#https://youtu.be/-s7e_Fy6NRU?t=1339
